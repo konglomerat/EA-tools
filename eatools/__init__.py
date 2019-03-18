8e9da01711b13c7bd61fe07ef4d1c63bc0ac3f2d
Bastian Loehrer <bastian.loehrer@tu-dresden.de>
2019-03-18 22:06:29 +0100

    own class for EA_ALL, basis for generation of PDF with bookings

--- eatools/__init__.py
+++ eatools/__init__.py
@@ -6,11 +6,12 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = [ 'https://www.googleapis.com/auth/drive.metadata.readonly',
-           'https://www.googleapis.com/auth/spreadsheets.readonly'   ]
 
-def get_credentials():
+# ==============================================================================
+# Google API functions
+# ------------------------------------------------------------------------------
+
+def get_credentials(SCOPES):
     """Authenticate and return credentials.
     Obtained from https://github.com/gsuitedevs/python-samples/blob/master/drive/quickstart/quickstart.py
     """
@@ -35,16 +36,93 @@ def get_credentials():
             pickle.dump(creds, token)
     return creds
 
+
 def start_drive(creds):
     """start the Google Drive service"""
     service = build('drive', 'v3', credentials=creds)
     return service
+
 
 def start_sheets(creds):
     """start the Google Sheets service"""
     service = build('sheets', 'v4', credentials=creds)
     return service
 
+
+# ==============================================================================
+# Utilities
+# ------------------------------------------------------------------------------
+
+def number2letter(number):
+    """Convert number to column letter (e.g. 0->"A", 1->"B", 26->"AA")"""
+    from string import ascii_uppercase
+    letters = []
+    while number >= 0:
+        letters.append(ascii_uppercase[number % 26])
+        number = number // 26 - 1
+        return ''.join(reversed(letters))
+
+
+
+# ==============================================================================
+# Class for things to be done with the EA_ALL table
+# ------------------------------------------------------------------------------
+
+class EA_ALL:
+
+    def __init__(self):
+        # If modifying these scopes, delete the file token.pickle.
+        self.SCOPES = [ 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                        'https://www.googleapis.com/auth/spreadsheets.readonly'   ]
+        self.creds = get_credentials(self.SCOPES)
+        self.drive = start_drive(self.creds)
+        self.sheets = start_sheets(self.creds)
+        #self.spreadsheet_id = "10-tp2M3fTWA5jEFl5DouieR-t9K3KOOYtJ9NiKaW6kw"  # EA_ALL 2017 Testkopie
+        self.spreadsheet_id = "1azciJGKkr3oIEmrFDnMCcQabPz3IeNMHxTJUmoPWbmQ"  # EA_ALL 2018
+        #self.sheet_id       = "1527830234"
+        self.sheet_name     = "EA_ALL"
+
+    def generate_pdf_of_bookings( self, year=2019, month=2 ):
+        """Erzeuge PDF mit allen Buchungen des Buchungszeitraums"""
+
+        # Parameters
+        sheets = self.sheets
+        column_name_dates = "Datum Überweisung"
+        spreadsheet_id = self.spreadsheet_id
+        sheet_name     = self.sheet_name
+
+        # Get Table column titles (from the first row)
+        sheet = sheets.spreadsheets()
+        result = sheet.values().get(spreadsheetId=spreadsheet_id, range=sheet_name+'!A1:1').execute()
+        values = result.get('values', [])
+        columns = { v:i for i,v in enumerate(values[0])  }
+
+        # Get row numbers and corresponding booking dates
+        column = number2letter(columns[column_name_dates])
+        rowID0 = 2
+        range = '{0}!{1}{2:d}:{1}'.format(sheet_name, column, rowID0)
+        result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range).execute()
+        bookingdates = result.get('values', [])
+        rowIDs = [ rowID0 + i for i,v in enumerate(bookingdates) ]
+
+        # Filter booking dates to obtain desired rows
+        i_select = [ i for i,v in enumerate(bookingdates) if v[0].startswith('{:04d}.{:02d}'.format(year,month)) ]
+        ranges = [ '{0}!A{1:d}:{1:d}'.format(sheet_name, rowIDs[i]) for i in i_select ]
+
+        # Get entries for the selected rows
+        result = sheet.values().batchGet(spreadsheetId=spreadsheet_id, ranges=ranges).execute()
+        valueRanges = result.get('valueRanges', [])
+
+        # Loop over the rows and do something
+        for valueRange in valueRanges:
+            values = valueRange.get('values', [])[0]
+            print(valueRange['range'])
+            print(values)
+            print('')
+
+        print("Jetzt muss noch das PDF erzeugt werden. TODO für Bastian.")
+
+        return
 
 
 # ==============================================================================
--- main.py
+++ main.py
@@ -2,16 +2,9 @@
 
 import eatools
 
-creds = eatools.get_credentials()
-drive = eatools.start_drive(creds)
-sheets = eatools.start_sheets(creds)
+# creds = eatools.get_credentials()
+# drive = eatools.start_drive(creds)
+# sheets = eatools.start_sheets(creds)
 
-# Hier wird nun "TESTKOPIE von EA_ALL KNGLMRT 2017" ausgelesen
-
-spreadsheet_id = "10-tp2M3fTWA5jEFl5DouieR-t9K3KOOYtJ9NiKaW6kw"
-# sheet_id       = "1527830234"
-sheet = sheets.spreadsheets()
-
-# Lese Kopfzeile aus der Tabelle um die Buchstaben zuordnen zu können
-result = sheet.values().get(spreadsheetId=spreadsheet_id, range='EA_ALL!A1:1').execute()
-values = result.get('values', [])
+EA_ALL = eatools.EA_ALL()
+EA_ALL.generate_pdf_of_bookings()
